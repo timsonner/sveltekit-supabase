5bbf519cc9e9bd14e189749ab1e300f000def5d9
timsonner <timsonner@gmail.com>
2022-01-23 06:53:08 +0000

    Working project

---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script>
+    import {user} from '$lib/sessionStore'
+    import {supabase} from '$lib/supabaseClient'
+    import Auth from '$lib/Auth.svelte'
+    import Profile from '$lib/Profile.svelte'
+
+    user.set(supabase.auth.user())
+
+    supabase.auth.onAuthStateChange((_, session) => {
+        user.set(session.user)
+    })
+</script>
+
+<div class="container" style="padding: 50px 0 100px 0;">
+    {#if $user}
+        <Profile />
+    {:else}
+        <Auth />
+    {/if}
+</div>
